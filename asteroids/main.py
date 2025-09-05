08d02b6c6d9d08db0adc5f7a05ec6b1b0c7c64c6
Oscar-I-M-G <its.habit.season@gmail.com>
2025-09-05 20:56:31 +0000

    Added shots and shot cooldown

--- asteroids/main.py
+++ asteroids/main.py
@@ -5,6 +5,7 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting Asteroids!")
@@ -17,9 +18,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    Asteroid.containers = (updatable, drawable,asteroids)
+    Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
     asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
---
+++ asteroids/shot.py
@@ -0,0 +1,18 @@
+## IMPORTS
+import pygame
+from circleshape import *
+from constants import *
+
+class Shot(CircleShape):
+
+    containers = ()
+
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+    def draw(self,screen):
+        pygame.draw.circle(screen, COLOR_SHOT, self.position, self.radius, width = 3)
+    
+    def update(self,dt):
+        self.position += self.velocity * dt
+
